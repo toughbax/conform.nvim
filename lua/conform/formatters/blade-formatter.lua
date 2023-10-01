61cd52fe25b9990ee66cacccd169976dc2bf475b
Andrew Baxter <andrew@toughbaxter.com>
2023-09-30 23:48:26 -0700

    Blade-Formatter

---
+++ lua/conform/formatters/blade-formatter.lua
@@ -0,0 +1,15 @@
+local util = require("conform.util")
+
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/shufo/blade-formatter",
+    description = "An opinionated blade template formatter for Laravel that respects readability",
+  },
+  command = util.find_executable({
+    "tools/blade_formatter/vendor/bin/blade-formatter",
+    "vendor/bin/blade-formatter",
+  }, "blade-formatter"),
+  args = { "$FILENAME" },
+  stdin = false,
+}
